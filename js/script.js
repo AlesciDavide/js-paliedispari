9e703fe97ed85b5e101c483b166661ba44db352f
AlesciDavide <davi.alesci@email.com>
2024-05-16 14:17:44 +0200

    inserito ciclo while e primo if di controllo

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ function parolaPalindroma(parola){
     let i = 0;
     let j = parolaInserita.length -1;
     let uguaglianzaLettere = true;
+    while(j!=0){
+        if(uguaglianzaLettere == true){
+            
+        }
+    }
 
-    
 }
